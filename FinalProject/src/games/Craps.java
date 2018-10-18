3ef8334c68345f8bca536070cd8b4008b72e7268
Phero Luong <Pl315135@students.davenport.k12.ia.us>
2018-10-18 08:49:26 -0500

    update

--- FinalProject/src/games/Craps.java
+++ FinalProject/src/games/Craps.java
@@ -1,10 +1,51 @@
-package games;
-
-public class Craps {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+//package games;
+//import java.util.Scanner;
+//import java.util.Random;
+//
+//public class Craps {
+//
+//	public static void craps(String[] args)
+//	// TODO Auto-generated method stub
+//	{
+//		  int craps;
+//		  int dice1 = 0;
+//		  int dice2 = 0;
+//		  int point = 0;
+//		  int newScore = 0;
+//		  int wins = 0;
+//		  int loss = 0;
+//		  Scanner inputDevice = new Scanner(System.in);
+//		  System.out.println("Welcome to Craps");
+//		  for (int i = 0; i < 10000; i++)
+//		  {
+//		    System.out.println ("roll the dices");
+//		    int score = roll (dice1, dice2);
+//		    System.out.println ("\n score " + score);
+//
+//		    if (score == 11 || score == 7)
+//		    {
+//		      System.out.println ("\n Score = " + score);
+//		      System.out.println ("you win");
+//		      wins = wins + 1;
+//		    }
+//		    if (score == 2 || score == 3 || score == 12)
+//		    {
+//		      System.out.println ("\n Score = " + score);
+//		      System.out.println ("you lose");
+//		      loss = loss + 1;
+//		    }
+//	}
+//
+//}
+//
+//	private static int roll(int dice1, int dice2) 
+//	// TODO Auto-generated method stub
+//	{
+//		Random randomGenerator = new Random ();
+//		  int dice1 = randomGenerator.nextInt (6) + 1;
+//		  int dice2 = randomGenerator.nextInt (6) + 1;
+//		  
+//		  System.out.println(" dice1 = " + dice1 + " dice2 = " + dice2 );
+//		return 0;
+//	}
+//}
--- FinalProject/src/games/Roulette.java
+++ FinalProject/src/games/Roulette.java
@@ -1,12 +1,39 @@
 package games;
-
+import java.util.Scanner;
+import java.util.Random;
 public class roulette {
-
-	public static void main(String[] args) 
+	
+	public roulette()
 	// TODO Auto-generated method stub
 	{
+		System.out.println("Welcome to the Roulette Game");
+		int userChoice;
+		int [] odds = { 0,1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35};
+		int [] evens = { 0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36};
 		
+		int[] randomNumbers = new int[36];
+		
+		for(int i = 0; i < randomNumbers.length; i++) 
+		{
+	          int i;
+			
+			[i] = (int)(Math.random() * 37);
 
-	}
-
+	    }
+		Scanner input = new Scanner(System.in);
+		do
+		{
+			System.out.println("Pick any number from 0 to 36 >>");
+			System.out.println("1-Odds\2- Evens >>");
+			int userChoice = input.nextInt();
+			
+			if(userChoice == odds[i])
+			{
+				System.out.println("")
+			}
+			
+		}
+		 
 }
+}
+		 
