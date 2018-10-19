59e9481b7244cb113919c7189f17d7e26617ee87
Phero Luong <Pl315135@students.davenport.k12.ia.us>
2018-10-19 09:10:38 -0500

    update

--- FinalProject/src/games/Roulette.java
+++ FinalProject/src/games/Roulette.java
@@ -7,33 +7,55 @@ public class roulette {
 	// TODO Auto-generated method stub
 	{
 		System.out.println("Welcome to the Roulette Game");
-		int userChoice;
-		int [] odds = { 0,1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35};
-		int [] evens = { 0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36};
+		int userGuess;
+		int computerSecret;
+		int red;
+		int black;
+	
+		String result;
+	
 		
-		int[] randomNumbers = new int[36];
-		
-		for(int i = 0; i < randomNumbers.length; i++) 
-		{
-	          int i;
-			
-			[i] = (int)(Math.random() * 37);
-
-	    }
+		computerSecret = (int)(Math.random() * 36 + 1);
 		Scanner input = new Scanner(System.in);
+		int quit = 0;
 		do
 		{
-			System.out.println("Pick any number from 0 to 36 >>");
-			System.out.println("1-Odds\2- Evens >>");
-			int userChoice = input.nextInt();
-			
-			if(userChoice == odds[i])
-			{
-				System.out.println("")
-			}
-			
+		System.out.println("Pick any number from 0 to 36 >>");
+		System.out.println("1-odds/2-evens");
+		System.out.println("1-red/2-black");
+		userGuess = input.nextInt();
+		
+		if(userGuess < computerSecret)
+		{
+			if()
+			result = "Your number is" + computerSecret;
 		}
-		 
+	  
+	else if(userGuess > computerSecret)
+	{
+		result = "Your number is" + computerSecret;
+	}
+	else if(userGuess == computerSecret)
+	{
+	result = "You win";
+	}
+	else 
+	{
+		result = "You lose";
+	}
+	 displayMessage(result);
+	 System.out.println("Do you want to quit ?/ n1- for yes / n2- for no >>");
+		
+	}while(1 != quit || userGuess != computerSecret);
+	}
+public static void displayMessage(String result)
+{
+	System.out.println(result);
 }
-}
+
+	}
+
+		
+
+	    
 		 
